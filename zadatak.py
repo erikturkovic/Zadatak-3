4a5ce54579ed085a47513b0a82d0a8dd129012a4
erikturkovic <eturkovic@student.unipu.hr>
2023-04-13 07:53:09 +0200

    Simulator ulaza

--- zadatak.py
+++ zadatak.py
@@ -8,9 +8,11 @@ def process_name(name):
     print(f"PID: {multiprocessing.current_process().pid}, ime: {name}, delay: {delay} sekundi")
 
 if __name__ == '__main__':
-    while True:
-        if input("Stisni Enter za početak, napiši 'end' za izlazak: ") == 'end':
-            break
+    input_number = 0
+    request_time = int(input("Upiši broj vremena razmaka kašnjena između zahtjeva "))
+    while input_number < 10:
+        time.sleep(request_time)
+        input_number+=1
         names = ['lmao','Erik', 'Filip', 'Nevup', 'Perica', 'Ela']
         processes = [multiprocessing.Process(target=process_name, args=(name,)) for name in names]
         for p in processes:
